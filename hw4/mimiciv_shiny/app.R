0dd3c653d300983200b40139f1d337a2a4eeda1a
Zhiyuan(Eric) Yu <hhyzy@g.ucla.edu>
2025-03-04 22:25:10 -0800

    created the second tab and finished Q2, haven't delete the codes for shiny app in the qmd file yet

--- hw4/mimiciv_shiny/app.R
+++ hw4/mimiciv_shiny/app.R
@@ -23,7 +23,6 @@ con_bq <- dbConnect(
   billing = "biostat-203b-2025-winter"
 )
 
-## Process & Save Patient Demographics
 ## Process & Save Patient Demographics 
 demographics_data <- tbl(con_bq, "patients") %>%
   select(subject_id, gender, anchor_age) %>%
@@ -35,10 +34,10 @@ demographics_data <- tbl(con_bq, "patients") %>%
   collect()
 saveRDS(demographics_data, "demographics_data.rds")
 
-## Process & Save Top 3 Diagnoses
-## Process & Save Top 3 Diagnoses (Now using HW3 filtering method)
+## Process & Save Top 3 Diagnoses 
 diagnoses_data <- tbl(con_bq, "diagnoses_icd") %>%
-  left_join(tbl(con_bq, "d_icd_diagnoses"), by = c("icd_code", "icd_version")) %>%
+  left_join(tbl(con_bq, "d_icd_diagnoses"), by = c("icd_code", 
+                                                   "icd_version")) %>%
   collect() %>%  # Collect everything first before filtering
   group_by(subject_id, long_title) %>%
   summarise(freq = n(), .groups = "drop") %>%  # Count occurrences
@@ -56,7 +55,7 @@ adt_data <- tbl(con_bq, "transfers") %>%
          outtime = as.POSIXct(outtime, format = "%Y-%m-%d %H:%M:%S"))
 saveRDS(adt_data, "adt_data.rds")
 
-## Process & Save Lab Events
+## Process & Save Labevents
 labevents_data <- tbl(con_bq, "labevents") %>%
   select(subject_id, charttime) %>%
   distinct() %>%
@@ -78,7 +77,7 @@ ui <- fluidPage(
                     selectInput("var_select", "Variable:", 
                                 choices = variable_groups[[1]]), 
                     
-                    uiOutput("slider_ui")  # Dynamic slider for age & lab measurements
+                    uiOutput("slider_ui")  
                   ),
                   mainPanel(
                     plotOutput("graph_plot"),  
@@ -94,7 +93,8 @@ ui <- fluidPage(
                   sidebarPanel(
                     textInput("subject_id", "Subject ID", ""),
                     actionButton("submit", "Submit"),
-                    selectInput("plot_type", "Select a plot:", choices = c("ADT", "ICU"))
+                    selectInput("plot_type", "Select a plot:", 
+                                choices = c("ADT", "ICU"))
                   ),
                   mainPanel(
                     plotOutput("patient_plot")
@@ -115,12 +115,16 @@ server <- function(input, output, session) {
   
   # Render slider input for `age_intime` and lab measurements
   output$slider_ui <- renderUI({
-    if (input$var_select %in% c("age_intime", variable_groups[["Lab Measurements"]])) {
-      sliderInput("value_range", paste("Select Range for", input$var_select, ":"),
+    if (input$var_select %in% c("age_intime", 
+                                variable_groups[["Lab Measurements"]])) {
+      sliderInput("value_range", paste("Select Range for", 
+                                       input$var_select, ":"),
                   min = min(mimic_icu_cohort[[input$var_select]], na.rm = TRUE),
                   max = max(mimic_icu_cohort[[input$var_select]], na.rm = TRUE),
-                  value = c(min(mimic_icu_cohort[[input$var_select]], na.rm = TRUE), 
-                            max(mimic_icu_cohort[[input$var_select]], na.rm = TRUE)))
+                  value = c(min(mimic_icu_cohort[[input$var_select]], 
+                                na.rm = TRUE), 
+                            max(mimic_icu_cohort[[input$var_select]], 
+                                na.rm = TRUE)))
     }
   })
   
@@ -135,7 +139,8 @@ server <- function(input, output, session) {
              x = input$var_select, y = "Count") +
         theme_minimal()
     } else {
-      ggplot(mimic_icu_cohort, aes(x = !!sym(input$var_select), fill = !!sym(input$var_select))) +
+      ggplot(mimic_icu_cohort, aes(x = !!sym(input$var_select), 
+                                   fill = !!sym(input$var_select))) +
         geom_bar() +
         labs(title = paste("Bar Plot of", input$var_select),
              x = input$var_select, y = "Count") +
@@ -160,7 +165,8 @@ server <- function(input, output, session) {
       colnames(summary_df) <- c("Category", "Count")
     }
     
-    datatable(summary_df, options = list(pageLength = 10, scrollX = TRUE, searching = FALSE))
+    datatable(summary_df, options = list(pageLength = 10, scrollX = TRUE, 
+                                         searching = FALSE))
   })
   
   # Display Missing Values Summary
@@ -186,56 +192,88 @@ server <- function(input, output, session) {
     diagnoses_data <- readRDS("diagnoses_data.rds")
     adt_data <- readRDS("adt_data.rds")
     labevents_data <- readRDS("labevents_data.rds")
+    icu_data <- readRDS("icu_data.rds")
     
-    # Get Patient Data
-    patient_demographics <- demographics_data %>%
-      filter(subject_id == !!subject_id)
-    
-    patient_diagnoses <- diagnoses_data %>%
-      filter(subject_id == !!subject_id) %>%
-      pull(long_title)
-    
-    patient_adt <- adt_data %>%
-      filter(subject_id == !!subject_id)
-    
-    patient_labevents <- labevents_data %>%
-      filter(subject_id == !!subject_id)
-    
-    patient_procedures <- tbl(con_bq, "procedures_icd") %>%
-      filter(subject_id == !!subject_id) %>%
-      left_join(tbl(con_bq, "d_icd_procedures"), 
-                by = c("icd_code" = "icd_code", "icd_version" = "icd_version")) %>%
-      collect() %>%
-      mutate(chartdate = as.POSIXct(chartdate, format = "%Y-%m-%d"))
-    
-    # Construct Demographic Title & Diagnoses Subtitle
-    patient_title <- paste("Patient", patient_demographics$subject_id[1], ", ",
-                           patient_demographics$gender[1], ", ",
-                           patient_demographics$anchor_age[1], "years old, ",
-                           patient_demographics$race[1])
-    
-    patient_subtitle <- paste(patient_diagnoses[1], patient_diagnoses[2], patient_diagnoses[3], sep = "\n")
-    
-    # Render Plot
-    output$patient_plot <- renderPlot({
-      ggplot() +
-        geom_segment(data = patient_adt,
-                     aes(x = intime, xend = outtime, y = "ADT", yend = "ADT", color = careunit),
-                     size = 4) +
-        geom_point(data = patient_labevents,
-                   aes(x = charttime, y = "Lab"),
-                   shape = 3, size = 2, color = "black") +
-        geom_point(data = patient_procedures,
-                   aes(x = chartdate, y = "Procedure", shape = sub(",.*", "", long_title)),
-                   size = 3, color = "black") +
-        scale_x_datetime(name = "Calendar Time") +
-        scale_y_discrete(name = NULL, limits = c("Procedure", "Lab", "ADT")) +
-        theme_bw() +
-        labs(title = patient_title, subtitle = patient_subtitle, x = "Time", y = "")
-    })
+    if (input$plot_type == "ADT") {
+      # Get Patient Data
+      patient_demographics <- demographics_data %>%
+        filter(subject_id == !!subject_id)
+      
+      patient_diagnoses <- diagnoses_data %>%
+        filter(subject_id == !!subject_id) %>%
+        pull(long_title)
+      
+      patient_adt <- adt_data %>%
+        filter(subject_id == !!subject_id)
+      
+      patient_labevents <- labevents_data %>%
+        filter(subject_id == !!subject_id)
+      
+      patient_procedures <- tbl(con_bq, "procedures_icd") %>%
+        filter(subject_id == !!subject_id) %>%
+        left_join(tbl(con_bq, "d_icd_procedures"), 
+                  by = c("icd_code" = "icd_code", 
+                         "icd_version" = "icd_version")) %>%
+        collect() %>%
+        mutate(chartdate = as.POSIXct(chartdate, format = "%Y-%m-%d"))
+      
+      # Construct Demographic Title & Diagnoses Subtitle
+      patient_title <- paste("Patient", 
+                             patient_demographics$subject_id[1], ", ",
+                             patient_demographics$gender[1], ", ",
+                             patient_demographics$anchor_age[1], "years old, ",
+                             patient_demographics$race[1])
+      
+      patient_subtitle <- paste(patient_diagnoses[1], patient_diagnoses[2], 
+                                patient_diagnoses[3], sep = "
+")
+      
+      output$patient_plot <- renderPlot({
+        ggplot() +
+          geom_segment(data = patient_adt,
+                       aes(x = intime, xend = outtime, y = "ADT", yend = "ADT", 
+                           color = careunit),
+                       size = 4) +
+          geom_point(data = patient_labevents,
+                     aes(x = charttime, y = "Lab"),
+                     shape = 3, size = 2, color = "black") +
+          geom_point(data = patient_procedures,
+                     aes(x = chartdate, y = "Procedure", 
+                         shape = sub(",.*", "", long_title)),
+                     size = 3, color = "black") +
+          scale_x_datetime(name = "Calendar Time") +
+          scale_y_discrete(name = NULL, limits = c("Procedure", "Lab", "ADT")) +
+          theme_bw() +
+          labs(title = patient_title, subtitle = patient_subtitle, x = "Time", 
+               y = "")
+      })
+    } else if (input$plot_type == "ICU") {
+      icu_data <- tbl(con_bq, "chartevents") %>%
+        filter(subject_id == !!subject_id, itemid %in% c(220045, 220180, 220179, 
+                                                         223761, 220210)) %>%
+        select(subject_id, stay_id, itemid, charttime, valuenum) %>%
+        collect() %>%
+        left_join(data.frame(itemid = c(220045, 220180, 220179, 223761, 220210),
+                             abbreviation = c("HR", "NBPd", "NBPs", 
+                                              "Temperature F", "RR")),
+                  
+                  by = "itemid") %>%
+        mutate(abbreviation = factor(abbreviation, 
+                                     levels = c("HR", "NBPd", "NBPs", 
+                                                "RR", "Temperature F")))
+      
+      output$patient_plot <- renderPlot({
+        ggplot(icu_data, aes(x = charttime, y = valuenum, 
+                             color = abbreviation)) +
+          geom_point() +
+          geom_line() +
+          facet_grid(abbreviation ~ stay_id, scales = "free") +
+          labs(title = paste("Patient", subject_id, "ICU stays - Vitals"), 
+               x = "Time", y = "Measurement") +
+          theme_minimal()
+      })
+    }
   })
 }
 
-
-# Run the application
 shinyApp(ui = ui, server = server)
